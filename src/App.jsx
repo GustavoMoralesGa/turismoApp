ba871bb1dad321bf20c3ca1be118b0ec2148d52c
Jorgemagus <jmolinaa@hotmail.es>
2023-02-27 23:28:06 -0300

    actualización gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
-import Login from './components/Login'
-import Register from './components/Register'
-import NotFound from './components/NotFound'
-import Home from './components/Home'
-import Tours from './components/Tours'
+import Login from './views/Login'
+import Register from './views/Register'
+import NotFound from './views/NotFound'
+import Home from './views/Home'
+import Tours from './views/Tours'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/tours" element={<Tours/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="*" element={<NotFound/>}/> 
+          <Route path="/" element={<Home />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
 
       </BrowserRouter>
