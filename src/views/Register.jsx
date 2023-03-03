ab492c4576680c773229419b44e4e5259532009a
Jorgemagus <jmolinaa@hotmail.es>
2023-03-02 22:16:15 -0300

    creación css, edición navbar, login y register

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -4,46 +4,51 @@ import Form from 'react-bootstrap/Form';
 import { Container } from 'react-bootstrap';
 import Alert from 'react-bootstrap/Alert';
 
-export default function Register () {
+export default function Register() {
     return (
         <>
-        <Container className="mt-2 p-2">
-            <Form className='text-center'>
-                
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Nombre completo</Form.Label>
-                    <Form.Control type="email" placeholder="Ingresar Nombre completo" />
-                </Form.Group>
+            <div className='bg-img d-flex align-items-center'>
+                <Container className="bg-white p-5 col-10 col-sm-6 col-md-3 m-auto">
+                    <div className='mt-3'>
+                        <h1>Registrarse</h1>
+                    </div>
+                    <Form className='text-left'>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Rut/Pasaporte</Form.Label>
-                    <Form.Control type="email" placeholder="Ingresar Rut/Pasaporte" />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Nombre completo</Form.Label>
+                            <Form.Control type="email" placeholder="Ingresar tu Nombre completo" />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Ingresar Email" />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Rut o Pasaporte</Form.Label>
+                            <Form.Control type="email" placeholder="Ingresa tu Rut o Pasaporte" />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Telefono</Form.Label>
-                    <Form.Control type="email" placeholder="Ingresar Telefono" />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Ingresa tu Email" />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Ingresar Contraseña" />
-                    </Form.Group>
-                    <Form.Group className="d-flex mb-3" controlId="formBasicCheckbox">
-                        <Form.Check className='text-start' type="checkbox" label="Confirmo que he leído y acepto la" />
-                        <Alert.Link className='text-start mx-1' href="#">Política de privacidad</Alert.Link>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Registrar
-                    </Button>
-            </Form>
-        </Container>
-           
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Teléfono</Form.Label>
+                            <Form.Control type="email" placeholder="Ingresa tu Teléfono" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password" placeholder="Ingresa tu Contraseña" />
+                        </Form.Group>
+                        <Form.Group className="d-flex mb-3" controlId="formBasicCheckbox">
+                            <Form.Check className='text-start' type="checkbox" label="Confirmo que he leído y acepto la" />
+                            <Alert.Link className='text-start mx-1' href="#">Política de privacidad</Alert.Link>
+                        </Form.Group>
+                        <Button variant="dark" type="submit">
+                            Registrar
+                        </Button>
+                    </Form>
+                </Container>
+            </div>
         </>
+
     )
 }
