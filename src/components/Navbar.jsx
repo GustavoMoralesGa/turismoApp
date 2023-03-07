fd59d13fc2f39b161b53660e738840ca0cb701d5
franco <fvargash@udd.cl>
2023-03-07 12:36:25 -0300

    cambie lnks por navlinks, agregue unos hovers, setActive y modal para el sidebar carrito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,61 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import { Navbar, Container } from "react-bootstrap"
+import { React, useState } from 'react'
+import { NavLink } from "react-router-dom"
+import { Navbar, Container, Button, Modal } from "react-bootstrap"
+import '../assets/css/style.css'
+
+const setActive = ({isActive}) => (isActive ? "active" : "noactive")
 
 export default function Navigation() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <>
-            <Navbar bg="light" variant="success">
+            <Navbar bg="light" variant="success" className='sm-12'>
                 <Container className="justify-content-start">
-                    <Navbar.Brand className="text-dark ms-3 text-decoration-none"><img src="https://jorgeendigital.com/wp-content/uploads/2023/03/Logotipo-asesoria-financiera-de-finanzas-moderno-geometrico-lila-e1677804069660.png" alt="Logo" width="250" /></Navbar.Brand>
-                    <Link to="/" className="text-dark ms-3 text-decoration-none">
+                    <Navbar.Brand className={"text-dark ms-3 text-decoration-none"}><img src="https://jorgeendigital.com/wp-content/uploads/2023/03/Logotipo-asesoria-financiera-de-finanzas-moderno-geometrico-lila-e1677804069660.png" alt="Logo" width="250" /></Navbar.Brand>
+                    <NavLink to="/" className={setActive} end>
                         Inicio
-                    </Link>
-                    <Link to="/tours" className="text-dark ms-3 text-decoration-none">
+                    </NavLink>
+                    <NavLink to="/tours" className={setActive}>
                         Tours
-                    </Link>
+                    </NavLink>
                 </Container>
 
                 <Container className="justify-content-end">
-                    <Link to="/login" className="text-dark ms-3 text-decoration-none">
+                    <NavLink to="/login" className={setActive}>
                         Iniciar sesión
-                    </Link>
-                    <Link to="/register" className="text-dark ms-3 text-decoration-none">
+                    </NavLink>
+                    <NavLink to="/register" className={setActive}>
                         Registrarse
-                    </Link>
-                    <Link to="/carrito" className="text-dark ms-3 text-decoration-none"><img src="https://cdn-icons-png.flaticon.com/512/4202/4202388.png" alt="Logo" width="30" />
-                    </Link>
+                    </NavLink>
+                    <NavLink className={setActive} onClick={handleShow}><img src="https://cdn-icons-png.flaticon.com/512/4202/4202388.png" alt="Logo" width="30" />
+                    </NavLink>
                 </Container>
             </Navbar>
+            
+
+        <Modal
+            className='modalcart'
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Carrito</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                Trekking 
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="danger" onClick={handleClose}>
+                    Eliminar Tour
+                </Button>
+                <NavLink to="/carrito" clasName={setActive}><Button variant="primary" onClick={handleClose}>Ir a pagar</Button></NavLink>
+                
+            </Modal.Footer>
+        </Modal>
         </>
     )
 }
