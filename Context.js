b8d196500aede77557c8844070557b9914b688c5
franco <fvargash@udd.cl>
2023-03-07 12:39:23 -0300

    cambio muy breve en el context {} y agregue en app el context provider y los componentes contex y cart

--- Context.js
+++ Context.js
@@ -1,5 +1,5 @@
 import { createContext } from "react"
 
-const Context = createContext(null)
+const Context = createContext({})
 
 export default Context
--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ import Register from './views/Register'
 import NotFound from './views/NotFound'
 import Home from './views/Home'
 import Tours from './views/Tours'
+import Context from '../Context'
+import Cart from './views/Cart'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tours" element={<Tours />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-      </BrowserRouter>
+      <Context.Provider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tours" element={<Tours />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/carrito" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
+      
     </div>
   )
 }
