87d74964c56989070e465a0bb3ab4f17fb4c7425
franco <fvargash@udd.cl>
2023-04-05 21:03:05 -0400

    cart

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import CartDetail from '../components/CartDetail';
 
 
 export default function Cart() {
     return (
-        <div className='d-flex align-items-center'>
-            <Container className="bg-white col-10 col-sm-6 col-md-3 m-auto container-form">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Trekking valle nevado</Card.Title>
-                        <Card.Text>
-                            Uno de los senderos mas visitados de la zona 
-                        </Card.Text>
-                        <Button className="textcenter" variant="primary">Ver más</Button>
-                    </Card.Body>
-                </Card>
-            </Container>
-
-        </div >
+        <CartDetail />
     )
 }
